d66bac2fda09496f4d0c5e736169f30b25085edb
Taylor McKinnon <taylor@uncannypacket.com>
2016-10-13 11:12:39 -0500

    Bump version: 0.3.4 → 0.3.5

--- smokesignal/__init__.py
+++ smokesignal/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Taylor McKinnon'
 __email__ = 'taylor@uncannypacket.com'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 from smokesignal.service import Service, Backend
 from smokesignal.client import Client
